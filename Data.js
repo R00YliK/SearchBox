bed7f9b558ce2f0650ce840db4977559aa0b9d2a
Roope Ylikulju <t2ylro00@students.oamk.fi>
2024-01-31 14:53:36 +0200

    Valmis

---
+++ Data.js
@@ -0,0 +1,54 @@
+export const DATA =[
+    {
+        id: '1',
+        firstname: 'Jouni',
+        lastname:'Juntunen'
+    },
+    {
+        id: '2',
+        firstname: 'Ville',
+        lastname: 'Ojala'
+    },
+    {
+        id: '3',
+        firstname: 'Robert',
+        lastname: 'Kulju'
+    },
+    {
+        id: '4',
+        firstname: 'Kankku',
+        lastname: 'Kankkunen'
+    },
+    {
+        id: '5',
+        firstname: 'Konna',
+        lastname: 'Koistinen'
+    },
+    {
+        id: '6',
+        firstname: 'Kallu',
+        lastname: 'Kallonen'
+    },
+    {
+        id: '7',
+        firstname: 'Jaakko',
+        lastname: 'Jabadaba'
+    },
+    {
+        id: '8',
+        firstname: 'Hullu',
+        lastname: 'Huuttis'
+    },
+    {
+        id: '9',
+        firstname: 'Tuttu',
+        lastname: 'Tuuttis'
+    },
+    {
+        id: '10',
+        firstname: 'Jäättis',
+        lastname: 'Vanamees'
+    }
+
+
+]
